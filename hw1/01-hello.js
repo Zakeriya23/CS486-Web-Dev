a0940d23354c9ed0bc33264bf9b74680c3b6a817
Zakeriya23 <zakeriyam23@gmail.com>
2024-10-17 21:46:22 -0700

    Homework completed

--- hw1/01-hello.js
+++ hw1/01-hello.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', loadPage);
 
 function loadPage(event) {
-  console.log('The page has now loaded.');
+  console.log("The page has now loaded.");
+  console.log("My name is Zakeriya Muhumed and I am taking CS463 to gain experience in Web Dev. I am Senior going for Bachelor's Degree in Computer.");
 }
